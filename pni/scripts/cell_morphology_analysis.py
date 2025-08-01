2da9d953187e371daad040d7e4d8904f1239a0b7
Ondrej Vala <ondrejvala2@seznam.cz>
2025-08-01 21:32:22 +0200

    Update paths

--- pni/scripts/cell_morphology_analysis.py
+++ pni/scripts/cell_morphology_analysis.py
@@ -1,4 +1,4 @@
 from pni.data.lif2tif import Lif2Tif
 
 lif2tif = Lif2Tif()
-lif2tif.convert("/home/ondrejvala/Documents/pni/example_data/ARPE19_nic_48H_01.lif","/home/ondrejvala/Documents/pni/example_out")
+lif2tif.convert("example_data/ARPE19_nic_48H_01.lif","example_out")
